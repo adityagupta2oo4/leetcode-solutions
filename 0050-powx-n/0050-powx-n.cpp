c0973f1687ae5d387e6c637f4dee6ee9dca74d5d
Aditya Gupta <ag751000@gmail.com>
2026-07-23 15:10:18 +0530

    Time: 0 ms (100%), Space: 8.8 MB (6.95%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        if(n == 0) return 1;
+        else if(n>0){
+            return x*pow(x,--n);
+        }
+        else{
+            return (1/x)*pow(x,++n);
+        }
+    }
+};
