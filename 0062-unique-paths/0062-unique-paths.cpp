ccebff8c86767596e7f3c76a43e74b2f3aa86e38
Aditya Gupta <ag751000@gmail.com>
2026-07-25 12:58:51 +0530

    Time: 0 ms (100%), Space: 8.2 MB (87.23%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,23 +1,25 @@
+vector<vector<int>> DP(101 , vector<int>(101,-1));
 class Solution {
 public:
 
     // applying Memoization since we have found overlapping nodes in recusrion tree
-    int solve(int row, int col,vector<vector<int>> &DP){
+    int solve(int row, int col){
 
         if(row == 0 && col == 0) return 1;
         if(row<0 || col < 0) return 0;
 
         if(DP[row][col] != -1) return DP[row][col];
 
-        int up = solve(row-1,col,DP);
-        int left = solve(row,col-1,DP);
+        int up = solve(row-1,col);
+        int left = solve(row,col-1);
 
         DP[row][col] = up+left;
         return up+left; 
     }
+
     int uniquePaths(int m, int n) {
-        vector<vector<int>> DP(m , vector<int>(n,-1));
-        return solve(m-1,n-1,DP);
+        
+        return solve(m-1,n-1);
     }
 };
 
