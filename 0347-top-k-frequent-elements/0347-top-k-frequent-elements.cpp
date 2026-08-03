3c913c00171de7440a76174ba4e85543fd794f73
Aditya Gupta <ag751000@gmail.com>
2026-08-03 21:34:10 +0530

    Time: 407 ms (8.19%), Space: 165.4 MB (5.87%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    int maxByValue(map<int,int> mp){
+
+        int key_max = 0; // assigning zero so that no garbage value
+        int value_max = INT_MIN;
+
+        for(auto it : mp){
+            if(it.second>value_max) {
+                value_max = it.second;
+                key_max = it.first;
+            }
+        }
+        return key_max;
+
+    }
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        map<int,int> hashMap;
+        
+        for(int i = 0 ; i<nums.size() ; i++){
+            hashMap[nums[i]] = 0;
+        }
+
+        for(int i = 0 ; i<nums.size() ; i++){
+            hashMap[nums[i]] += 1;
+        }
+        vector<int> ans;
+        for(int i = 0 ; i<k ; i++){
+
+            int max  = maxByValue(hashMap);
+            ans.push_back(max);
+            hashMap.erase(max);
+
+        }
+
+        for(auto it : hashMap ){
+            cout<<it.first<<" , "<<it.second<<endl;
+        }
+        
+
+        return ans;
+
+    }
+};
