ac5295be126ecd5fcaed0c34c5c462548c359672
Aditya Gupta <ag751000@gmail.com>
2026-08-03 10:44:07 +0530

    Time: 57 ms (54.82%), Space: 24 MB (26.38%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -10,36 +10,38 @@ class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
 
-        ListNode* temp = headA;
-        int countA=0,countB=0;
-        while(temp){
-            countA++;
-            temp = temp->next;
-        }
-        temp = headB;
-        while(temp){
-            countB++;
-            temp = temp->next;
-        }
-
-        cout<<countA<<","<<countB;
 
         ListNode* cur1 = headA;
         ListNode* cur2 = headB;
 
-        int it = 0;
-        if(countA>countB){
-            it = countA-countB;
-            for(int i = 0; i<it ; i++){
-                cur1 = cur1->next;
+        
+        int countA=0,countB=0;
+        
+        while(cur1 || cur2){
+            if(cur1){
+                countA++;
+                cur1=cur1->next;
             }
-        }
-        else{
-            it = countB-countA;
-            for(int i = 0; i<it ; i++){
+            
+            if(cur2){
+                countB++;
                 cur2 = cur2->next;
             }
         }
+
+        cur1 = headA;
+        cur2 = headB;
+
+        bool flagA = countA>countB ? true : false;
+        int it = flagA ? countA - countB : countB - countA;
+
+        ListNode* temp = flagA ? cur1 : cur2;
+
+        for(int i = 0 ; i<it ; i++) temp = temp->next;
+
+        if(flagA) cur1 = temp;
+        else cur2 =temp;
+
 
         while(cur1 && cur2){
             if(cur1 == cur2 ) return cur1;
