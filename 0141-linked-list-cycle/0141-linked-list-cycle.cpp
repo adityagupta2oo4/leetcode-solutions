9687da8e4fe8bd1af7e54fc153ab898cea7cb4ac
Aditya Gupta <ag751000@gmail.com>
2026-08-04 12:01:24 +0530

    Time: 12 ms (42.12%), Space: 11.9 MB (54.48%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,16 +11,14 @@ public:
     bool hasCycle(ListNode *head) {
 
         //edge case
-        if(head == nullptr || head->next == nullptr) return false;
+        if(head == nullptr ) return false;
         ListNode* cur = head;
         int x = 1000000 ;//choosing value which is out of range of  Node.val just to mark it visited
 
         while(cur){
 
-            if (cur->next && cur->next->val == x)
+            if (cur->next && cur->next->val == x) //checking curr-> nest exist or not then accessing its value
                 return true;
-
-
             cur->val = x;
             cur = cur->next;
         }
